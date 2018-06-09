f40943eb170492b3bb1a2c856ec2b7b69b8becef
KarlaLG91 <37052911+KarlaLG91@users.noreply.github.com>
2018-06-08 19:35:23 -0500

    Module for mapping

---
+++ 003a-bwa/config.mk
@@ -0,0 +1,4 @@
+## Here we declare the variables that are going to be used by mkfile.
+
+## Define the indexed reference the aligner will map the reads to. (HG19)
+REFERENCE="/media/genomics/disco1/klozano_bkp/PEISV/000-prepare_test_data/001a-BWA_index_genome/results/hg19.minimized.fa"
