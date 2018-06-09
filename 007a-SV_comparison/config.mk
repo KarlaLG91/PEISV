45a729dc5b3de3ba6fb464deb51360d26e810bc6
KarlaLG91 <37052911+KarlaLG91@users.noreply.github.com>
2018-06-08 19:39:44 -0500

    Comparison of estimated SVs with simulated SVs

---
+++ 007a-SV_comparison/config.mk
@@ -0,0 +1,10 @@
+## Here we declare the variables that are going to be used by mkfile
+
+# Define the path to the simulated SV file
+#SIMsv="/media/genomics/disco1/klozano_bkp/PEISV/001a-RSVSim/results/hg19.minimized_DEL_DUP_INV100bp/deletions.csv"
+#SIMsv="/media/genomics/disco1/klozano_bkp/PEISV/001a-RSVSim/results/hg19.minimized_DEL_DUP_INV100bp/tandemDuplications.csv"
+SIMsv="/media/genomics/disco1/klozano_bkp/PEISV/001a-RSVSim/results/hg19.minimized_DEL_DUP_INV100bp/inversions.csv"
+#SIMsv="/media/genomics/disco1/klozano_bkp/PEISV/001a-RSVSim/results/hg19.minimized_INS/insertions.csv"
+
+# Define the tolerance accepted when comparing the (approximate) positions in estimated SVs with the exact simulated breakpoints.
+TOL="20"
